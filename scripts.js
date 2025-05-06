ce8825247f4e231ab1de7893517a5e4d784945e5
Mohammadebrahim_2005 <mohammadebrahim340720@gmail.com>
2025-05-06 18:01:15 +0330

    Add calculator logic part 3

--- scripts.js
+++ scripts.js
@@ -12,7 +12,7 @@ let operation;
 document.addEventListener("DOMContentLoaded", () => 
     {
     const rainContainer = document.querySelector(".rain");
-    const dropsCount = 500; // تعداد قطرات
+    const dropsCount = 400; // تعداد قطرات
 
     for (let i = 0; i < dropsCount; i++) 
     {
@@ -51,3 +51,65 @@ function clearDisplay()
     currDisplay.innerText = "";
     prevDisplay.innerText = "";
 }
+
+function compute(operand)
+{
+    let result;
+    const previousValue = parseFloat(prevDisplay.innerText);
+    const currentValue = parseFloat(currDisplay.innerText);
+
+    if (isNaN(previousValue) || isNaN(currentValue)) return;
+
+    switch (operation) {
+        case "+" :
+        result = previousValue + currentValue;
+        break;
+
+        case "-" :
+        result = previousValue - currentValue;
+        break;
+
+        case "*" :
+        result = previousValue * currentValue;
+        break;
+
+        case "/" :
+        result = previousValue / currentValue;
+        break;
+
+
+        default :
+        result;
+    }
+    currDisplay.innerText = result;
+}
+
+numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+        appendNumber(number.innerText);
+    });
+});
+
+operands.forEach((operand) => 
+{
+    operand.addEventListener("click" , () => 
+    {
+        chooseOperation(operand.innerText);
+    });
+});
+
+clearBtn.addEventListener("click" , () => 
+{
+    clearDisplay();
+});
+
+equalBtn.addEventListener("click" , () => 
+{
+    compute();
+    prevDisplay.innerText = "";
+});
+
+delBtn.addEventListener("click" , () => 
+{
+    currDisplay.innerText = currDisplay.innerText.slice(0,-1);
+});
